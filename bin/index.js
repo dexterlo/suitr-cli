3038b7a6ca98e89da16901cc187f6723d4b948fe
Dexter Lo <dexterlo@gmail.com>
2021-05-05 08:03:31 -0700

    Add driver-to-destination index mapping combos

--- bin/index.js
+++ bin/index.js
@@ -6,4 +6,13 @@ const fileArgs = process.argv.slice(2);
 const destinations = fileToArray(fileArgs[0]);
 const drivers = fileToArray(fileArgs[1]);
 
-console.log([].concat([...destinations],[...drivers]));
+// build array of possible driver-to-destination index mappings (element: [Driver, Destination])
+// decided to keep it light (no row data or suitability score logic yet)
+// leave it until we build the actual routes
+let combos = [];
+for (let i = 0; i < drivers.length; i++) {
+  for (let j = 0; j < destinations.length; j++) {
+    combos.push([i, j]);
+  }
+}
+console.log(combos);
